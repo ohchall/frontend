8ed57a8ccf403bd2340bdb8852aeb26cc5b27d02
dragonrain5761 <dragonrain5761@gmail.com>
2023-07-31 21:16:29 +0900

    Todo작성 페이지 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useFetchTodos } from "../api/TodoApi";
+import { styled } from "styled-components";
 
 function MyPage() {
-  return <div>MyPage</div>;
+  const { data, isLoading, isError } = useFetchTodos();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error occurred while fetching data.</div>;
+  }
+
+  return (
+    <>
+      <h2>My Page</h2>
+      <ul>
+        {data.map((todo) => (
+          <TodosDiv key={todo.id}>
+            <div>Title: {todo.title}</div>
+            <div>Content: {todo.content}</div>
+            <div>Date: {todo.date}</div>
+          </TodosDiv>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default MyPage;
+
+const TodosDiv = styled.div`
+  border: 1px solid black;
+`;

2e40aad29fb48003fbbfc3745a7f44d394b8b101
dragonrain5761 <dragonrain5761@gmail.com>
2023-08-02 08:59:46 +0900

    feat:todo 모달 추가

---
+++ src/components/TodoModal.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useAddTodoMutation } from "../api/TodoApi";
+
+function TodoModal({ onRequestClose }) {
+  const addTodoMutation = useAddTodoMutation();
+  const [todo, setTodo] = useState({
+    title: "",
+    content: "",
+    date: "",
+  });
+
+  const onChangeTodoHandler = (e) => {
+    const { name, value } = e.target;
+    console.log(value);
+    setTodo({
+      ...todo,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(todo);
+    addTodoMutation.mutate(todo, {
+      onSuccess: (data) => {
+        console.log("저장 성공:", data);
+        setTodo({
+          title: "",
+          content: "",
+          date: "",
+        });
+        onRequestClose();
+      },
+      onError: (error) => {
+        console.error("저장 실패:", error);
+      },
+    });
+  };
+
+  return (
+    <>
+      <h2>운동 기록 TODO</h2>
+      <div>
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={todo.title}
+          onChange={(e) => onChangeTodoHandler(e)}
+        ></input>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="content"
+          placeholder="content"
+          value={todo.content}
+          onChange={(e) => onChangeTodoHandler(e)}
+        ></input>
+      </div>
+      <div>
+        <input
+          type="date"
+          name="date"
+          placeholder="date"
+          value={todo.date}
+          onChange={(e) => onChangeTodoHandler(e)}
+        ></input>
+      </div>
+      <button onClick={handleSubmit}>Todo 추가</button>
+    </>
+  );
+}
+
+export default TodoModal;

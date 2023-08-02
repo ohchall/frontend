c5113051b26bd1dd923625ee2956bd7010aaed64
Art-mj1 <mjmjpc@naver.com>
2023-08-02 09:21:45 +0900

    크루 업로드 포스트 이미지 formdata 구축

---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import ReactDatePicker, { registerLocale } from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from "date-fns/esm/locale";
+import range from "lodash/range";
+import getYear from "date-fns/getYear";
+import getMonth from "date-fns/getMonth";
+
+registerLocale("ko", ko);
+
+const DatePicker=() => {
+ const [startDate, setStartDate] = useState(new Date());
+ const years = range(2000, getYear(new Date()) + 1, 1);
+ const months = [
+   "January",
+   "February",
+   "March",
+   "April",
+   "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December",
+ ];
+ return (
+   <ReactDatePicker
+     renderCustomHeader={({
+       date,
+       changeYear,
+       changeMonth,
+       decreaseMonth,
+       increaseMonth,
+       prevMonthButtonDisabled,
+       nextMonthButtonDisabled,
+     }) => (
+       <div
+         style={{
+           margin: 10,
+           display: "flex",
+           justifyContent: "center",
+         }}
+       >
+         <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+           {"<"}
+         </button>
+         <select
+           value={getYear(date)}
+           onChange={({ target: { value } }) => changeYear(value)}
+           locale="ko"
+         >
+           {years.map((option) => (
+             <option key={option} value={option} >
+               {option}
+             </option>
+           ))}
+         </select>
+
+         <select
+           value={months[getMonth(date)]}
+           onChange={({ target: { value } }) =>
+             changeMonth(months.indexOf(value))
+           }
+           locale="ko"
+         >
+           {months.map((option) => (
+             <option key={option} value={option}locale="ko">
+               {option}
+             </option>
+           ))}
+         </select>
+
+         <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+           {">"}
+         </button>
+       </div>
+     )}
+     selected={startDate}
+     onChange={(date) => setStartDate(date)}
+     locale={ko}
+   />
+ );
+};
+export default DatePicker;

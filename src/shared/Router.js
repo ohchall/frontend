8a283cbe3430eb174389c6390e165811551a10e2
leolego03 <leolego03@gmail.com>
2023-08-02 12:40:28 +0900

    feat: Crew 상세페이지 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,11 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import MyPage from "../pages/MyPage";
 import CrewWritePage from "../pages/CrewWritePage";
-import Mypage from "../pages/MyPage";
-import MyCrew from "../components/MyCrew";
 import CrewPage from "../pages/CrewPage";
 import CrewDetailPage from "../pages/CrewDetailPage";
 import MainLayout from "../components/layout/MainLayout";
+import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SignUpPage";
 
 function Router() {
   return (
@@ -16,7 +16,6 @@ function Router() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<MainPage />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/todo/write" element={<TodoWritePage />} />
           <Route path="/crew/write" element={<CrewWritePage />} />
           <Route path="/crew" element={<CrewPage />} />
           <Route path="/crew/detail" element={<CrewDetailPage />} />

ceaf18fa3d3ce8c33a74cd8ed466c23886365e34
leolego03 <leolego03@gmail.com>
2023-08-11 14:11:48 +0900

    design: add icon to footer section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,21 +1,45 @@
 import {
+  FooterSection,
   LinkContainer,
   StyledLink,
-  FooterSection,
-  HomeIconStyle,
-} from "./Footer.style";
-import HomeIcon from "../../assets/OhChalleHome.svg";
+  StyledFiSmile,
+  StyledFiSearch,
+  StyledLuUser,
+  StyledLuUsers } from "./Footer.style";
+import { ReactComponent as HomeIcon } from "../../assets/HomeIcon.svg";
 function Footer() {
   return (
     <FooterSection>
       <LinkContainer>
-        <StyledLink to="/crew">크루</StyledLink>
-        <StyledLink to="/">커뮤니티</StyledLink>
-        <StyledLink to="/">
-          <HomeIconStyle src={HomeIcon} alt="homelogo" />
+        <StyledLink to="/crew">
+          <StyledFiSmile />
+          <span>크루</span>
         </StyledLink>
-        <StyledLink to="/">검색</StyledLink>
-        <StyledLink to="/mypage">마이페이지</StyledLink>
+
+        <StyledLink to="/">
+          <StyledLuUsers />
+          <span>커뮤니티</span>
+        </StyledLink>
+        
+        <StyledLink to="/">
+          <HomeIcon
+            width="42.6px"
+            height="24px"
+            viewBox="0 6 42.6 24"
+            // viewBox="0 0 49 35"
+          />
+          <span>홈</span>
+        </StyledLink>
+        
+        <StyledLink to="/">
+          <StyledFiSearch />
+          <span>검색</span>
+        </StyledLink>
+        
+        <StyledLink to="/mypage">
+          <StyledLuUser />
+          <span>마이페이지</span>
+        </StyledLink>
       </LinkContainer>
     </FooterSection>
   );

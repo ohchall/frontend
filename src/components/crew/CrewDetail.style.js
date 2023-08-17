4f270ba3a4d194dcb8d2d28e3a260dbb735780d8
leolego03 <leolego03@gmail.com>
2023-08-17 22:40:39 +0900

    feat: add image to crew detail component

--- src/components/crew/CrewDetail.style.js
+++ src/components/crew/CrewDetail.style.js
@@ -1,17 +1,29 @@
 import styled from 'styled-components';
 
+export const ImageWrapper = styled.div`
+  width: 100%;
+  height: 310px;
+  display: flex;
+  justify-content: center;
+  background-color: #EEEEEE;
+  overflow: hidden;
+
+  & > img {
+    height: 100%;
+  }
+`;
+
 export const CrewDetailBlock = styled.div`
   width: 100%;
-  padding: 16px;
 `;
 
 export const Header = styled.div`
-  width: 100%;
+  width: calc(100 - 32px);
   border: 1px solid #5B5B5B;
   border-radius: 10px;
   padding: 16px;
   font-size: 14px;
-  margin-bottom: 5px;
+  margin: 16px;
 
   & > div {
     display: flex;
@@ -28,19 +40,23 @@ export const Header = styled.div`
 `;
 
 export const MapWrapper = styled.div`
-  width: 100%;
+  width: calc(100% - 32px);
   height: 200px;
   border-radius: 10px;
   background-color: #f2f2f2;
-  margin-bottom: 16px;
+  margin: 0 16px 16px 16px;
   overflow: hidden;
 `;
 
 export const ButtonWrapper = styled.div`
+  width: 100%;
 
   & button {
     width: 100%;
     height: 54px;
+    font-size: 16px;
+    color: #FFFFFF;
+    background-color: #111111;
     border: none;
   }
 `;

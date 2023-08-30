3620210e623c042e853ca2793da6aa079dbce6a0
leolego03 <leolego03@gmail.com>
2023-08-30 20:48:32 +0900

    design: fix join button disappearance on mobile

--- src/components/crew/CrewJoinButton.style.js
+++ src/components/crew/CrewJoinButton.style.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
 
 export const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 60px;
   width: 100%;
+  height: 60px;
   z-index: 9;
 
   & > button {
     width: 100%;
-    height: 54px;
+    height: 100%;
     font-size: 16px;
     color: #FFFFFF;
     background-color: #111111;
     border: none;
     cursor: pointer;
+  }
+
+  @media (max-width: 430px) {
+    position: fixed;
   }
 `;
--- src/pages/crew/CrewDetailPage.style.ts
+++ src/pages/crew/CrewDetailPage.style.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const CrewDetaiPageBlock = styled.div`
   width: 100%;
-  height: calc(100% - 174px);
+  height: calc(100% - 180px);
   overflow: auto;
   margin: 0 auto;
 
   &::-webkit-scrollbar {
     display:none;
   }
-`
+`;
